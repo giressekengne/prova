d6377510ec29224a3c221e49c424a39d56a53a41
Giresse Kengne <gigatore97@gmail.com>
2022-11-03 13:07:58 +0100

    mod1

---
+++ Citta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace prova
+{
+    public class Città
+    {
+        // Attributi e incapsulamento
+        public string _nome { get; set; }
+        public string _sindaco { get; set; }
+        int capacitàMassima { get; set; }
+        public int[] _cittadino = new int[10];
+        int i = 0;
+
+        // Costruttore per creare un'istanza della citta
+        public Città(string nome, string sindaco, int capacità)
+        {
+            _nome = nome;
+            _sindaco = sindaco;
+            capacitàMassima = capacità;
+
+        }
+
+
+        // Metodo per creare un cittadino maggiorene in una Città 
+        public void addCittadino(Cittadino cittadino)
+        {
+            cittadino.dataDiNas();
+            DateTime result = new DateTime();
+            DateTime.TryParse(cittadino.dataDiNascita, out result);
+            if (result.Year <= DateTime.Now.Year - 18)
+            {
+                if (i <= capacitàMassima)
+
+                {
+                    _cittadino[i] = cittadino._id;
+                    cittadino.città = this;
+                    i += 1;
+                }
+                else { Console.WriteLine("La città non ammette più cittadini"); }
+            }
+            else Console.WriteLine("Il cittadino ha meno di 18 anni quindi non può entrare");
+        }
+
+        // Metodo per Stampare un cittadino ( mediante l'uso del FOREACH molto comodo per la stampa
+        // delle liste e Array)
+        public void stampaCittadino()
+        {
+
+            foreach (int cittadini in _cittadino)
+            {
+                if (i <= cittadini)
+                    Console.WriteLine(cittadini);
+
+            }
+        }
+
+
+
+
+      
+    }
+}
---
+++ Cittadino.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace prova
+{
+    public class Cittadino
+    {
+
+        // Attributi ed Incapsulamento
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string sesso { get; set; }
+        public string dataDiNascita { get; set; }
+        public int _id { get; set; }
+        public Città città;
+
+
+        //Costruttore per Creare instanza un istanza del cittadino
+        public Cittadino(string nome, string cognome, string sesso, int id)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.sesso = sesso;
+            this._id = id;
+
+        }
+
+        // Metodo per Prendere la data di nascita da Tastiere (Comparabile alla scanf in c)
+        public void dataDiNas()
+        {
+            Console.WriteLine("Inserire la data di nascita");
+            dataDiNascita = Console.ReadLine();
+        }
+
+        
+    }
+}
